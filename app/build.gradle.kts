319750ecdffee4ad7a27a4ee9d8b1d10297d48eb
gomeyyy <razinrahimi@gmail.com>
2025-03-16 03:11:40 +0800

    Ejas2 login page skit, dgn initialise firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
