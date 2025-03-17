03f432d040aa74aa0fe00d5bc41931285276cdf5
Win11 <ftineffia@gmail.com>
2025-03-18 03:54:37 +0800

    register page, account setting, weekly planner

--- app/src/main/java/com/razinrahimi/remine/ui/AccountSetting.java
+++ app/src/main/java/com/razinrahimi/remine/ui/AccountSetting.java
@@ -1,27 +1,25 @@
-package com.razinrahimi.remine;
+package com.razinrahimi.remine.ui;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.razinrahimi.remine.R;
 
-public class MainActivity extends AppCompatActivity {
+public class AccountSetting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.title_login), (v, insets) -> {
+        setContentView(R.layout.activity_account_setting);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
 }
