256fd5a176e945b2c4b6a2b060157388d496ee5a
gomeyyy <razinrahimi@gmail.com>
2025-03-18 20:48:01 +0800

    Pages flow,add spinner

--- app/src/main/java/com/razinrahimi/remine/ui/MasterTimetable.java
+++ app/src/main/java/com/razinrahimi/remine/ui/MasterTimetable.java
@@ -1,5 +1,6 @@
 package com.razinrahimi.remine.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,16 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
 
 import com.razinrahimi.remine.R;
 
 public class MasterTimetable extends AppCompatActivity {
+
+    Button goToAddTaskBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_master_timetable);
+
+        goToAddTaskBtn = findViewById(R.id.goToAddTaskButton);
+        goToAddTaskBtn.setOnClickListener(view -> startActivity(new Intent(this, FirestoreTestingActivity.class)));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
