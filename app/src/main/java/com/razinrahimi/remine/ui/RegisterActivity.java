2b598479207f8dcb2e254d1bc30814785dbd99e3
gomeyyy <razinrahimi@gmail.com>
2025-03-25 06:02:33 +0800

    More comments, GL presentation

--- app/src/main/java/com/razinrahimi/remine/ui/RegisterActivity.java
+++ app/src/main/java/com/razinrahimi/remine/ui/RegisterActivity.java
@@ -20,7 +20,9 @@ import com.razinrahimi.remine.data.Users;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    //Define UI components
     private EditText registerEmailInput, registerPasswordInput,confirmPasswordInput, usernameInput;
+    //Define database variables
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
@@ -30,9 +32,11 @@ public class RegisterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+        //Init Firebase authentication and firestore
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
+        //Find UI components using findViewById
         registerEmailInput = findViewById(R.id.registerEmailInput);
         registerPasswordInput = findViewById(R.id.registerPasswordInput);
         confirmPasswordInput = findViewById(R.id.confirm_password_input);
@@ -41,7 +45,9 @@ public class RegisterActivity extends AppCompatActivity {
         Button registerUserButton = findViewById(R.id.registerUserButton);
         Button loginPageButton = findViewById(R.id.loginPageButton);
 
+        //call registerUser method on click
         registerUserButton.setOnClickListener(view -> registerUser());
+        //go to login page onclick
         loginPageButton.setOnClickListener(view -> startActivity(new Intent(this, LoginActivity.class)));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register_page_layout), (v, insets) -> {
@@ -52,19 +58,25 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser() {
+        //Retrieve user input and change into String
         String email = registerEmailInput.getText().toString().trim();
         String password = registerPasswordInput.getText().toString().trim();
         String confirmPassword = confirmPasswordInput.getText().toString().trim();
         String username = usernameInput.getText().toString().trim();
 
+        //Check if required fields are empty
         if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
             Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
             return;
         }
+        //Check if confirm password and password the same
         if (!password.equals(confirmPassword)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
             return;
         }
+        //Firebase authentication function to create new user using email and password
+        //User Id is automatically assigned by firebase
+        //Built-in exception handler for firebase and firestore operations
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
@@ -72,6 +84,8 @@ public class RegisterActivity extends AppCompatActivity {
                         String userID = mAuth.getCurrentUser().getUid();
                         Users user = new Users(userID, email, username);
 
+                        //Store User's insensitive details into firestore users collection
+                        //Go to login page upon successful registration
                         db.collection("users").document(userID)
                                 .set(user)
                                 .addOnSuccessListener(aVoid -> {
@@ -79,9 +93,11 @@ public class RegisterActivity extends AppCompatActivity {
                                     startActivity(new Intent(this, LoginActivity.class));
                                     finish();
                                 })
+                                //Notify user if user data not stored in firestore
                                 .addOnFailureListener(e ->
                                         Toast.makeText(this, "Failed to store user data: " + e.getMessage(), Toast.LENGTH_SHORT).show());
                     } else {
+                        //notify user if registration failed
                         Toast.makeText(this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
